506dd758a5d131dfc99313a65f7d47e2b50a1040
jackson Croy <Jrcroy0607@gmail.com>
2020-03-05 11:01:55 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080
+const path = require("path")
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT)
+})
