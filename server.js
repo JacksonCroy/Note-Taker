2de65292326f3075eeaf80ad0d465765985f9194
jackson Croy <Jrcroy0607@gmail.com>
2020-03-06 10:10:22 -0700

    added base server js file

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require("path")
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 
 
